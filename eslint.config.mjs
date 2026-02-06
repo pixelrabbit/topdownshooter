29580757e7ad63e7021cb5658a4ef7c8a7546740
pixelrabbit <dangovea@gmail.com>
2026-02-06 16:14:32 -0600

    lint fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import prettier from "eslint-plugin-prettier/recommended";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", "src/engine"] },
+  { ignores: ["dist", "src/engine/**/*.ts"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
     files: ["**/*.{ts,tsx}"],
